336421dbfec257ae8cc30c9dee21cb95750f710c
Sonu Kumar <sonu.k@deltax.com>
2022-09-10 01:32:33 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
 import React from "react";
 import Cart from "./components/Cart";
 import Navbar from "./components/Navbar";
+import { getFirestore } from "firebase/firestore";
+import { collection, getDocs, onSnapshot } from "firebase/firestore";
 
 class App extends React.Component {
   constructor(params) {
     super();
     this.state = {
-      products: [
-        {
-          id: 1,
-          price: 99,
-          title: "Headphone",
-          qty: 1,
-          img: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1000&q=60",
-        },
-        {
-          id: 2,
-          price: 9999,
-          title: "Mobile Phone",
-          qty: 10,
-          img: "https://images.unsplash.com/photo-1601784551446-20c9e07cdbdb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bW9iaWxlJTIwcGhvbmV8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60",
-        },
-        {
-          id: 3,
-          price: 101010,
-          title: "Apple Laptop",
-          qty: 5,
-          img: "https://images.unsplash.com/photo-1593642702821-c8da6771f0c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGFwdG9wc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1000&q=60",
-        },
-      ],
+      products: [],
+      isLoading:true
     };
+  }
+
+  componentDidMount() {
+    const db = getFirestore();
+    // getDocs(collection(db, "products")).then((querySnapshot) => {
+    //   const products = querySnapshot.docs.map((doc) => {
+    //     var data = doc.data();
+    //     data.id = doc.id;
+    //     return data;
+    //   });
+    //   this.setState({
+    //     products,
+    //     isLoading:false
+    //   })
+    // });
+    onSnapshot(collection(db, "products"), (querySnapshot) => {
+      const products = querySnapshot.docs.map((doc) => {
+        var data = doc.data();
+        data.id = doc.id;
+        return data;
+      });
+      this.setState({
+        products,
+        isLoading:false
+      })
+    });
   }
 
   handleIncreaseQuantity = (product) => {
@@ -68,33 +75,36 @@ class App extends React.Component {
   getCartCount = () => {
     const { products } = this.state;
     let count = 0;
-    products.forEach(prod => {
+    products.forEach((prod) => {
       count += prod.qty;
     });
     return count;
-  }
+  };
 
   getCartTotal = () => {
     const { products } = this.state;
     let total = 0;
-    products.forEach(prod => {
+    products.forEach((prod) => {
       total += prod.qty * prod.price;
     });
     return total;
-  }
+  };
 
   render() {
-    const { products } = this.state;
+    const { products, isLoading } = this.state;
     return (
       <div className="App">
-        <Navbar count={this.getCartCount()}/>
+        <Navbar count={this.getCartCount()} />
+        {isLoading && <h1>Loading...</h1>}
         <Cart
           products={products}
           onIncreaseQuantity={this.handleIncreaseQuantity}
           onDecreaseQuantity={this.handleDecreaseQuantity}
           onDeleteItem={this.handleDeleteItem}
         />
-        <div style={{fontSize:20, padding:10}}>Total: {this.getCartTotal()}</div>
+        <div style={{ fontSize: 20, padding: 10 }}>
+          Total: {this.getCartTotal()}
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { initializeApp } from "firebase/app";
 import './index.css';
 import App from './App';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD1Cnej1aKutlXwNgxd0R6tZlHIKXrLqqM",
+  authDomain: "cart-8f451.firebaseapp.com",
+  projectId: "cart-8f451",
+  storageBucket: "cart-8f451.appspot.com",
+  messagingSenderId: "671584221323",
+  appId: "1:671584221323:web:cab69e7526822f33c73c8e"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
